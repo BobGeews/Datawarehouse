b14a3a68084e2d851fd0f48754fd9e0b5257d956
BobGeews <ossi.hakkinen@hotmail.fi>
2019-03-14 08:46:46 +0200

    going to delete person from list.

---
+++ personExampleCorrect/personExampleCorrect/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using personExampleCorrect.Repositories;
+using System.Runtime.InteropServices;
+using System.Data.SqlClient;
+
+namespace personExampleCorrect
+{
+    class Program
+    {
+        private static readonly PersonRepository _personRepository = new PersonRepository();
+        static void Main(string[] args)
+        {
+            //testing database read
+            PersonRepository personRepository = new PersonRepository();
+            //var persons = personRepository.Read();
+            //var person = personRepository.ReadById(1234);
+            CreatePerson();
+            ReadByCity();
+            for (int i = 0; i< 100; i++)
+            {
+                ReadById(i * 2);
+            }
+            //foreach(var p in person)
+            //{
+            //    Console.WriteLine($"{p.Id} {p.FirstName} {p.LastName}");
+            //}
+            
+        }
+        static void ReadByCity()
+        {
+            var persons = _personRepository.ReadByCity("Lontoo");
+
+            foreach (var p in persons)
+            {
+
+                Console.WriteLine($"{p.Id} {p.FirstName} {p.LastName} {p.City}");
+            }
+            Console.WriteLine("------------------------------");
+        }
+        static void ReadById (long id)
+        {
+            var person = _personRepository.ReadById(id);
+            if (person == null)
+            {
+                Console.WriteLine($"Asiakasta numerolla {id} ei löydy.");
+
+            }
+            else
+                Console.WriteLine($"{person.Id} {person.FirstName} {person.City}");
+        }
+        static void CreatePerson()
+        {
+            Person person = new Person();
+            person.FirstName = "james";
+            person.LastName = "Bond";
+            person.City = "Lontoo";
+            person.ShoeSize = 45;
+            _personRepository.Create(person);
+        }
+
+    }
+}
---
+++ personExampleCorrect/personExampleCorrect/Repositories/PersonRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using personExampleCorrect;
+
+namespace personExampleCorrect.Repositories
+{
+    class PersonRepository : IPersonRepository
+    {
+        private readonly PersontestdbContext _persontestdbContext = new PersontestdbContext();
+
+
+        public void Create(Person person)
+        {
+            string sql = $"INSERT INTO PERSON (Firstname, LastName, City, Shoesize" +
+                $"VALUES ({person.FirstName}, {person.LastName}, {person.City}, {person.ShoeSize})";
+
+            _persontestdbContext.Add(person);
+            _persontestdbContext.SaveChanges();
+
+        }
+
+        public void Delete(long id)
+        {
+            //delete*from person where id ={id} sql lause
+            var deletedPerson = ReadById(id);
+            if (deletedPerson != null)
+            {
+                _persontestdbContext.Remove(deletedPerson);
+                _persontestdbContext.SaveChanges();
+                Console.WriteLine("Tiedot poistettu onnistuneesti.");
+            }
+            else
+            {
+                Console.WriteLine("Tiedon poisto EI onnistunut.");
+            }
+        }
+
+        public List<Person> ReadByCity(string city)
+        {
+            var persons = _persontestdbContext.
+                Person.
+                Where(p => p.City == city).
+                ToListAsync().
+                Result;
+            return persons;
+        }
+
+        public Person ReadById(long id)
+        {
+            var person = _persontestdbContext.Person.Find(id);
+            return person;
+        }
+
+        public void Update(long id, Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
